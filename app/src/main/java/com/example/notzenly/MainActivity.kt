d5fd6de5741fc220d785643dc12a3fdfbec51e6f
karcerr <roma.mershon@mail.ru>
2024-03-02 00:25:19 +0500

    02.03.2024 - fixed permissions? kind of

--- app/src/main/java/com/example/notzenly/MainActivity.kt
+++ app/src/main/java/com/example/notzenly/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.notzenly
 
 
-import android.content.Intent
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import android.os.Bundle
@@ -13,19 +13,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
+
 //import org.osmdroid.config.Configuration.*
 
 
 class MainActivity : AppCompatActivity() {
     private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
-
+    private val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+    private var has_permission = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //Handling Permissions:
-        val permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_NETWORK_STATE)
-        requestPermissionsIfNecessary(permissions)
         setContentView(R.layout.activity_main)
-        if (!isLocationEnabled()) {
+
+        //Handling Permissions:
+        requestPermissionsIfNecessary(permissions)
+
+        if (!isLocationEnabled() or !has_permission) {
             findViewById<Button>(R.id.btnGrantPermissionsAndEnableLocation).setOnClickListener {
                 requestLocation()
             }
@@ -61,21 +64,34 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        val permissionsToRequest = ArrayList<String>()
-        var i = 0
-        while (i < grantResults.size) {
-            permissionsToRequest.add(permissions[i])
-            i++
+
+        val permissionsToRequest = ArrayList<String>();
+        permissions.forEach { permission ->
+            if (ContextCompat.checkSelfPermission(this, permission)
+                != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted
+                permissionsToRequest.add(permission);
+            }
         }
-        if (permissionsToRequest.size > 0) {
+        if (permissionsToRequest.isNotEmpty()) {
             ActivityCompat.requestPermissions(
                 this,
                 permissionsToRequest.toTypedArray(),
                 REQUEST_PERMISSIONS_REQUEST_CODE
             )
+        } else{
+            has_permission = true
         }
     }
 
+    private fun hasAllPermissionsGranted(grantResults: IntArray): Boolean {
+        for (grantResult in grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                return false
+            }
+        }
+        return true
+    }
 
     private fun requestPermissionsIfNecessary(permissions: Array<String>) {
         val permissionsToRequest = ArrayList<String>();
@@ -91,6 +107,8 @@ class MainActivity : AppCompatActivity() {
                 this,
                 permissionsToRequest.toTypedArray(),
                 REQUEST_PERMISSIONS_REQUEST_CODE);
+        } else{
+            has_permission = true
         }
     }
 }
