1b63f15ed8bce69e997358d3c1bd195f02a14c6b
karcerr <roma.mershon@mail.ru>
2024-05-01 16:05:32 +0500

    01/05/2024 Added gradient background for uploaded compressed images (just like in insta holy shit). Map intersect counter v1

--- app/src/main/java/tagme/CustomIconOverlay.kt
+++ app/src/main/java/tagme/CustomIconOverlay.kt
@@ -21,6 +21,10 @@ class CustomIconOverlay(
     private val clickListener: ((CustomIconOverlay) -> Unit)?
 
 ) : Overlay(context) {
+    private var intersectCount = 0
+    private var visible = true
+    private var accountedForIntersect = false
+    private var overlayScale = calculateScaleFactor(drawable)
     var mapView: MapView? = null
     private val textPaint = Paint().apply {
         color = Color.BLACK
@@ -42,7 +46,7 @@ class CustomIconOverlay(
 
             val distance = sqrt((x - it.x).toDouble().pow(2.0) + (y - it.y).toDouble().pow(2.0))
 
-            val iconRadius = (drawable.intrinsicWidth * calculateScaleFactor(drawable) / 2).toFloat()
+            val iconRadius = (drawable.intrinsicWidth * overlayScale / 2).toFloat()
 
             if (distance <= iconRadius) {
                 clickListener?.invoke(this)
@@ -62,14 +66,36 @@ class CustomIconOverlay(
 
         val rotation = -(mapView?.mapOrientation ?: 0f)
 
+        point?.let { currentPoint ->
+            var intersected = false
+            mapView.overlays?.forEach { overlay ->
+                if (overlay is CustomIconOverlay && overlay.visible && doOverlaysIntersect(this, overlay, currentPoint)) {
+                    intersected = true
+                }
+            }
+
+            visible = !intersected
+            intersectCount = 0
+            if (visible) {
+                accountedForIntersect = true
+                mapView.overlays?.forEach { overlay ->
+                    if (overlay is CustomIconOverlay && !overlay.accountedForIntersect && doOverlaysIntersect(this, overlay, currentPoint)) {
+                        intersectCount += 1
+                        overlay.accountedForIntersect = true
+                    }
+                }
+            } else {
+                accountedForIntersect = false
+            }
+        }
+
+        if(!visible) return
         point?.let {
             canvas.save()
             canvas.rotate(rotation, it.x.toFloat(), it.y.toFloat())
-
-            val scaleFactor = calculateScaleFactor(drawable)
-
-            val scaledWidth = (drawable.intrinsicWidth * scaleFactor).toInt()
-            val scaledHeight = (drawable.intrinsicHeight * scaleFactor).toInt()
+            overlayScale = calculateScaleFactor(drawable)
+            val scaledWidth = (drawable.intrinsicWidth * overlayScale).toInt()
+            val scaledHeight = (drawable.intrinsicHeight * overlayScale).toInt()
             drawable.setBounds(it.x - scaledWidth / 2, it.y - scaledHeight / 2,
                 it.x + scaledWidth / 2, it.y + scaledHeight / 2)
             val strokePaint = Paint().apply {
@@ -106,11 +132,13 @@ class CustomIconOverlay(
 
             val speedText = "${speed.toInt()}m/s"
             val nameText = name
-
+            val intersectCountText = "+$intersectCount"
             if (speed.toInt() != 0)
-                canvas.drawText(speedText, it.x.toFloat(), (it.y + scaledHeight / 2 + 45).toFloat(), textPaint)
+                canvas.drawText(speedText, it.x.toFloat(), (it.y + scaledHeight / 2 + 65).toFloat(), textPaint)
             canvas.drawText(nameText, it.x.toFloat() - textPaint.measureText(nameText) / 2, (it.y - scaledHeight / 2 - 10).toFloat(), textPaint)
-
+            if (intersectCount != 0) {
+                canvas.drawText(intersectCountText, it.x.toFloat(), (it.y + scaledHeight / 2 + 65).toFloat(), textPaint)
+            }
             canvas.restore()
         }
     }
@@ -125,6 +153,23 @@ class CustomIconOverlay(
             1f
         }
     }
+    private fun doOverlaysIntersect(overlay1: CustomIconOverlay, overlay2: CustomIconOverlay, currentPoint: Point): Boolean {
+        if (overlay2 != overlay1) {
+            val otherProjection = mapView!!.projection
+            val otherPoint = otherProjection?.toPixels(overlay2.location, null)!!
+            val distance = sqrt(
+                (currentPoint.x - otherPoint.x).toDouble().pow(2.0) +
+                        (currentPoint.y - otherPoint.y).toDouble().pow(2.0)
+            )
+            val iconRadius = (drawable.intrinsicWidth * overlayScale / 2)
+            val otherIconRadius = (overlay2.drawable.intrinsicWidth * calculateScaleFactor(overlay2.drawable) / 2)
+            return (distance <= iconRadius + otherIconRadius)
+        }
+        return false
+    }
+
+
+
 
     private fun dpToPx(dp: Float): Int {
         val scale = context.resources.displayMetrics.density
@@ -147,5 +192,4 @@ class CustomIconOverlay(
         drawable = newDrawable
         mapView?.invalidate()
     }
-
 }
