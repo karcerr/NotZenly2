a7549f5c01ce43b8326cec98b330030453c65dfa
karcerr <roma.mershon@mail.ru>
2024-05-21 22:42:36 +0500

    21/05/2024 Added OpenStreetMap Attribution. Added geo story viewing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.tagme"
         minSdk = 31
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.000001"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
